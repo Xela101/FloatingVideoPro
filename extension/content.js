1848873a043c0dd7e3999d6935213bd45e2c8ab7
Xela <whitexela101@gmail.com>
2021-02-15 11:35:35 +0000

    Fix video removed edge case

--- extension/content.js
+++ extension/content.js
@@ -2,7 +2,7 @@
   let videos = Array.from(document.getElementsByTagName("video"));
   if(videos.length>0){
     let videoInPictureModeIndex = videos.findIndex(video => document.pictureInPictureElement === video);
-    let nextVideoForInPictureMode = videoInPictureModeIndex === -1 ? 0 : videoInPictureModeIndex === videos.length-1 ? 0 : ++videoInPictureModeIndex;
+    let nextVideoForInPictureMode = videoInPictureModeIndex === -1 ? 0 : videoInPictureModeIndex >= videos.length-1 ? 0 : ++videoInPictureModeIndex;
     videos[nextVideoForInPictureMode].requestPictureInPicture();
   }
 })();
